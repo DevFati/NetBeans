97d01256b97b0bacda9c55ffaad2af46f30e8a69
Fatima <fatima.mortahilchachou@gmail.com>
2024-09-24 21:23:05 +0200

    subida ej

--- Actividad2_1/src/actividades/ejercicio6.java
+++ Actividad2_1/src/actividades/ejercicio6.java
@@ -5,10 +5,31 @@
  */
 package actividades;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Tarde
  */
 public class ejercicio6 {
-    
+    public static void main(String[] args) {
+        /*Realiza un programa que pida al usuario una palabra y cuente cuántas vocales tiene.*/
+        
+        Scanner teclado=new Scanner(System.in);
+        
+        System.out.println("Introduce una palabra: ");
+        String palabra=teclado.next();
+        char vocales[]={'a','e','i','o','u'};
+        int contador=0;
+        for(int i=0;i<palabra.length();i++){
+            for(int j=0;j<vocales.length;j++){
+                if(palabra.charAt(i)==vocales[j]){
+                    contador++;
+                }
+            }
+            
+        }
+        System.out.println("La palabra tiene "+contador+" vocales.");
+        
+    }
 }
---
+++ Actividad2_1/src/actividades/ejercicio7.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividades;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tarde
+ */
+public class ejercicio7 {
+    public static void main(String[] args) {
+        /*Crea un programa que pida 5 calificaciones, las guarde en un array y luego calcule y
+muestre el promedio de las mismas.*/
+        
+        Scanner teclado=new Scanner(System.in);
+        int [] calificaciones=new int [5];
+        for(int i=0;i<5;i++){
+            
+        }
+        
+    }
+}
