e05bb2f59d138b1abf4e265f79f52693ca538295
Fatima <fatima.mortahilchachou@gmail.com>
2024-10-07 19:54:04 +0200

    Subida ej

--- GestionnClientes/src/gestionnclientes/gui/PantallaPrincipal.java
+++ GestionnClientes/src/gestionnclientes/gui/PantallaPrincipal.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package gestionnclientes;
+package gestionnclientes.gui;
 
-import gestionnclientes.DialogoAlta;
+import gestionnclientes.gui.DialogoAlta;
 
 /**
  *
