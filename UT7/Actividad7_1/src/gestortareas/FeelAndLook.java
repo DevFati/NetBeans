f1f27e7558121c4b666b4a0deb90b99347a1ea0b
Fatima <fatmortcha@gmail.com>
2025-02-10 10:22:40 +0100

    10/02/2025 COMMIT
    Additions:
        - I finished the exercise "Actividad7_1" and rhe exercises of the unit 7 that must be shown to the teacher in class

---
+++ UT7/Actividad7_1/src/gestortareas/FeelAndLook.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestortareas;
+
+import javax.swing.JFrame;
+import org.jvnet.substance.SubstanceLookAndFeel;
+
+/**
+ *
+ * @author Fatima Mortahil Chachou
+ */
+public class FeelAndLook {
+
+    public static void main(String[] args) {
+        //Aqui configuramos para que las ventanas usen "Look and Feel" 
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        //Cambiamos el estilo visual. 
+        SubstanceLookAndFeel.setSkin("org.jvnet.substance.skin.CremeCoffeeSkin");
+
+        //Instanciamos la ventana LogIn y la hacemos visible
+        LogIn v = new LogIn();
+        v.setVisible(true);
+    }
+}
